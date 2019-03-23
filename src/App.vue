948c0d06a47b9f2c67116b2c52e4b071c2961cdc
TSY <t@tsy6.com>
2019-03-23 23:19:25 +0800

    test jenkins

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png">
         <HelloWorld msg="Welcome to hxkj.vip"/>
-        <div>test jenkins again！</div>
+        <div>test jenkins again！！</div>
     </div>
 </template>
 
